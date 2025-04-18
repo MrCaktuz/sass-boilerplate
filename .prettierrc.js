09bb413d1eb2f09cd3af92a715d7e369d235293d
Mr Caktuz <claessens.mathieu@gmail.com>
2025-04-17 22:14:58 -0400

    Move mixins to specific file

---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  printWidth: 80, // default
+  tabWidth: 2, // Default
+  useTabs: false, // Default
+  singleQuote: true, // Best practice
+  jsxSingleQuote: false, // Default
+  bracketSameLine: false, // Default
+  bracketSpacing: true, // Default
+  embeddedLanguageFormatting: "auto", // Default
+  insertPragma: false, // Default
+  requirePragma: false, // Default
+  endOfLine: "lf", // Default
+  arrowParens: "always", // Default
+  htmlWhitespaceSensitivity: "css", // Default
+  proseWrap: "preserve", // Default
+  quoteProps: "as-needed", // Default
+  singleAttributePerLine: false, // Default
+  semi: true, // Default
+  trailingComma: "es5", // Default
+};
